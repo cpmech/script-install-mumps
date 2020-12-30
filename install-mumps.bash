2de914097f0e3c10bb8ce75538a7984f4ec457bf
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-12-30 11:38:45 +1000

    Compile .a and .so libs

--- install-mumps.bash
+++ install-mumps.bash
@@ -10,6 +10,7 @@ sudo () {
 # options
 INTEL=${1:-"false"}
 SIMPLE=${2:-"false"}
+ZNUMBERS=${3:-"false"}
 
 # constants
 MUMPS_VERSION="5.3.5"
@@ -19,16 +20,18 @@ HERE=`pwd`
 PDIR=$HERE/patch
 
 # download and exctract the source code
-curl http://deb.debian.org/debian/pool/main/m/mumps/$MUMPS_GZ -o /tmp/$MUMPS_GZ
-rm -rf $MUMPS_DIR
+#curl http://deb.debian.org/debian/pool/main/m/mumps/$MUMPS_GZ -o /tmp/$MUMPS_GZ
+rm -rf /tmp/$MUMPS_DIR
 cd /tmp
 tar xzf /tmp/$MUMPS_GZ
-rm /tmp/$MUMPS_GZ
+#rm /tmp/$MUMPS_GZ
 
 # patch and compile
 cd $MUMPS_DIR
+patch -u libseq/Makefile $PDIR/libseq/Makefile.diff
 patch -u PORD/lib/Makefile $PDIR/PORD/lib/Makefile.diff
 patch -u src/Makefile $PDIR/src/Makefile.diff
+patch -u Makefile $PDIR/Makefile.diff
 if [ "${INTEL}" = "true" ]; then
   echo ">>>>>>>>>>>>>>>>>>>>>> using Intel MKL/MPI <<<<<<<<<<<<<<<<<<<<<<<<<<<"
   if [ "${SIMPLE}" = "true" ]; then
@@ -47,7 +50,9 @@ else
   fi
 fi
 make d
-make z
+if [ "${ZNUMBERS}" = "true" ]; then
+  make z
+fi
 chmod -x lib/*
 
 # copy include and lib files to the right places
@@ -57,10 +62,12 @@ LIBDIR=$PREFIX/lib/mumps
 sudo mkdir -p $INCDIR/
 sudo mkdir -p $LIBDIR/
 sudo cp -av include/*.h $INCDIR/
-sudo cp -av lib/libpord.so $LIBDIR/
-sudo cp -av lib/libdmumps.so $LIBDIR/
-sudo cp -av lib/libzmumps.so $LIBDIR/
-sudo cp -av lib/libmumps_common.so $LIBDIR/
+sudo cp -av lib/libpord*.* $LIBDIR/
+sudo cp -av lib/libdmumps*.* $LIBDIR/
+if [ "${ZNUMBERS}" = "true" ]; then
+  sudo cp -av lib/libzmumps*.* $LIBDIR/
+fi
+sudo cp -av lib/libmumps_common*.* $LIBDIR/
 cd $HERE
 sudo cp -av mumps.ld.so.conf /etc/ld.so.conf.d/
 sudo ldconfig
--- uninstall-mumps-be-careful.bash
+++ uninstall-mumps-be-careful.bash
@@ -2,5 +2,5 @@
 
 sudo rm -rf /usr/local/include/mumps
 sudo rm -rf /usr/local/lib/mumps
-sudo rm /etc/ld.so.conf.d/mumps.ld.so.conf
+sudo rm -f /etc/ld.so.conf.d/mumps.ld.so.conf
 sudo ldconfig
