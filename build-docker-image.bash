67b08cf44e9f751a5f64c71de88a189bccb8c78d
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-01-19 14:52:51 +1000

    Improve output of build docker script

--- build-docker-image.bash
+++ build-docker-image.bash
@@ -6,16 +6,27 @@ set -e
 INTEL=${1:-"OFF"}
 MPI=${2:-"OFF"}
 
-# suffix
-SFX="_open"
+# image name
+NAME="mumps_open"
 if [ "${INTEL}" = "ON" ]; then
-    SFX="_intel"
+    NAME="mumps_intel"
+fi
+if [ "${MPI}" = "ON" ]; then
+    NAME="${NAME}_mpi"
 fi
 
 # build Docker image
-docker build --no-cache -t mumps$SFX . \
+docker build --no-cache -t $NAME . \
     --build-arg INTEL=$INTEL \
     --build-arg MPI=$MPI
 
 # remove dangling images
 docker images -q -f "dangling=true" | xargs docker rmi 2>/dev/null
+
+echo
+echo
+echo
+echo "... SUCCESS ..."
+echo
+echo "... image ${NAME} created ..."
+echo
