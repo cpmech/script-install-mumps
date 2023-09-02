15ab4b048812b47414f00ab7d5de000832e4c198
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-02 12:29:22 +1000

    Update files

--- install-mumps.bash
+++ install-mumps.bash
@@ -8,65 +8,58 @@ sudo () {
   "$@"
 }
 
-# arguments
-OMP=${1:-"OFF"}
-DYN=${2:-"OFF"}
-ZNUMBERS=${3:-"OFF"}
-
 # options
-VERSION="5.4.0"
+VERSION="5.6.1"
 PREFIX="/usr/local"
 INCDIR=$PREFIX/include/mumps
 LIBDIR=$PREFIX/lib/mumps
 PDIR=`pwd`/patch
 
-# selection
-SELECTION=".open.seq"
-if [ "${OMP}" = "ON" ]; then
-    SELECTION="${SELECTION}.omp"
-fi
-if [ "${DYN}" = "ON" ]; then
-    SELECTION="${SELECTION}.dyn"
-fi
-
-# extract the source code into /tmp dir
-MUMPS_GZ=MUMPS_$VERSION.tar.gz
+# download the source code
+MUMPS_GZ=mumps_$VERSION.orig.tar.gz
 MUMPS_DIR=MUMPS_$VERSION
-cp source/$MUMPS_GZ /tmp/
 cd /tmp
 if [ -d "$MUMPS_DIR" ]; then
     echo "... removing previous $MUMPS_DIR directory"
     rm -rf $MUMPS_DIR
 fi
+if [ -f "$MUMPS_GZ" ]; then
+    echo "... using existing $MUMPS_GZ file"
+else
+    curl http://deb.debian.org/debian/pool/main/m/mumps/$MUMPS_GZ -o $MUMPS_GZ
+fi
+
+# extract the source code into /tmp dir
 tar xzf $MUMPS_GZ
 cd $MUMPS_DIR
 
 # patch Makefiles
-patch -u libseq/Makefile $PDIR/libseq/Makefile.diff
-patch -u PORD/lib/Makefile $PDIR/PORD/lib/Makefile.diff
-patch -u src/Makefile $PDIR/src/Makefile.diff
 patch -u Makefile $PDIR/Makefile.diff
-cp $PDIR/Makefile.inc$SELECTION.txt Makefile.inc
+cp $PDIR/Makefile.inc Makefile.inc
 
-# compile double
-make d
-
-# compile complex
-if [ "${ZNUMBERS}" = "ON" ]; then
-    make z
-fi
-
-# copy include and lib files to the right places
+# create output directoryes and copy include files
 sudo mkdir -p $INCDIR/
 sudo mkdir -p $LIBDIR/
 sudo cp -av include/*.h $INCDIR/
-sudo cp -av lib/libpord*.* $LIBDIR/
-sudo cp -av lib/libdmumps*.* $LIBDIR/
-sudo cp -av lib/libmpiseq*.* $LIBDIR/
-if [ "${ZNUMBERS}" = "ON" ]; then
-    sudo cp -av lib/libzmumps*.* $LIBDIR/
-fi
-sudo cp -av lib/libmumps_common*.* $LIBDIR/
+
+# compile double (static)
+make d
+sudo cp -av lib/lib*.a $LIBDIR/
+
+# compile double (shared)
+make clean # << important
+make dshared
+sudo cp -av lib/lib*.so $LIBDIR/
+
+# compile complex (static)
+make clean # << important
+make z
+sudo cp -av lib/lib*.a $LIBDIR/
+
+# compile complex (shared)
+make clean # << important
+make zshared
+sudo cp -av lib/lib*.so $LIBDIR/
 
 # update ldconfig
 echo "${LIBDIR}" | sudo tee /etc/ld.so.conf.d/mumps.conf >/dev/null
